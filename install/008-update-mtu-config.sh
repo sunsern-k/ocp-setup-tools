e9087bf1915ce95970ce914fa4de7b0be1548f6b
Sunsern Kunakornvaroj <skunakor@redhat.com>
2021-12-02 16:58:14 +0700

    update mtu

--- install/008-update-mtu-config.sh
+++ install/008-update-mtu-config.sh
@@ -1,4 +1,14 @@
-echo "Backuping up the network config file: ${OCP_DIR}/manifests/cluster-network-03.config.yaml"
-cp ${OCP_DIR}/manifests/cluster-network-03.config.yaml ${OCP_BACKUP}
-yq e -i '.defaultNetwork.openshiftSDNConfig.mtu = $mtu' ${OCP_DIR}/manifests/cluster-network-03.config.yaml
-grep -B 3 'mtu' ${OCP_DIR}/manifests/cluster-network-03.config.yaml
+if ! "$mtu" = "" ]
+then
+cat <<END > ${OCP_DIR}/manifests/cluster-network-03.config.yaml
+apiVersion: operator.openshift.io/v1
+kind: Network
+metadata:
+  name: cluster
+spec:
+  defaultNetwork:
+    openshiftSDNConfig:
+      mtu: $mtu
+END
+chmod 640 ${OCP_DIR}/manifests/cluster-network-03.config.yaml
+fi
