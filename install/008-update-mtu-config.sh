28fb604373d24e4da93a874d003202026baf7b58
Sunsern Kunakornvaroj <skunakor@redhat.com>
2021-12-02 17:03:18 +0700

    Update messages when configuring mtu

--- install/008-update-mtu-config.sh
+++ install/008-update-mtu-config.sh
@@ -1,5 +1,7 @@
 if ! "$mtu" = "" ]
 then
+  echo "Updating the mtu: $mtu -  ${OCP_DIR}/manifests/cluster-network-03.config.yaml"
+
 cat <<END > ${OCP_DIR}/manifests/cluster-network-03.config.yaml
 apiVersion: operator.openshift.io/v1
 kind: Network
@@ -10,5 +12,11 @@ spec:
     openshiftSDNConfig:
       mtu: $mtu
 END
-chmod 640 ${OCP_DIR}/manifests/cluster-network-03.config.yaml
+
+  chmod 640 ${OCP_DIR}/manifests/cluster-network-03.config.yaml
+  ls ${OCP_DIR}/manifests/cluster-network-03.config.yaml
+  echo ${OCP_DIR}/manifests/cluster-network-03.config.yaml
+
+else
+ echo "Skipped - the mtu is not specified - assumed 1450 which is the default value"
 fi
