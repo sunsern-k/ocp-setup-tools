8405f027a058cff83dd4552798aebd3063a65b51
Sunsern Kunakornvaroj <skunakor@redhat.com>
2021-12-06 21:32:34 +0700

    Fixed a type on environemnt example and update the backup install-config

--- install/003-backup-install-config.sh
+++ install/003-backup-install-config.sh
@@ -1,2 +1,3 @@
+echo "Backing up ${INSTALL_CONFIG}.."
 cp $INSTALL_CONFIG  ${OCP_BACKUP}/install-config.yaml.org
-cat ${OCP_BACKUP}/install-config.yaml.org
+ls -l ${OCP_BACKUP}/install-config.yaml.org
