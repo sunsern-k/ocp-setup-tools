9a913712271ec4d13735c219d108a5829a911c3e
Sunsern Kunakornvaroj <skunakor@redhat.com>
2021-12-08 22:06:55 +0700

    clean up script

--- tools/set_cpu_memory_bootstrap.sh
+++ tools/set_cpu_memory_bootstrap.sh
@@ -7,9 +7,7 @@ m=$2
 cpu=${c:=2}
 mem=${b:=8096}
 
-govc vm.change -vm $RHCOS_WORKER -c 4 -m 16384
+govc vm.change -vm $RHCOS_ODF -c 16 -m 32768
 
 govc vm.change -vm $RHCOS_BOOTSTRAP -c $cpu -m $mem
-govc vm.change -vm $RHCOS_INFRA_INT -c 4 -m 16384
-govc vm.change -vm $RHCOS_INFRA_MON -c 8 -m 32768
-govc vm.change -vm $RHCOS_ODF -c 16 -m 32768
+
