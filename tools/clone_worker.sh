00c68d92bfc31283d49447e00da8d749bc5de7b8
Sunsern Kunakornvaroj <skunakor@redhat.com>
2021-12-09 14:01:16 +0700

    Update the worker cloning script

--- tools/clone_worker.sh
+++ tools/clone_worker.sh
@@ -1,16 +1,16 @@
 for i in {1..2}
 do
   # DNS record
-  vmfqdn="infra0"${i}.${OCP_DOMAIN}
+  vmfqdn="worker0${i}.${OCP_DOMAIN}"
 
   # VM name on vCenter 
   # Ex: VMname is a DNS record
-  # vmname_prefix="infra0"
+  # vmname_prefix="odf0"
   # vmname_suffix=".${OCP_DOMAIN}"
   # vmname="${vmname_prefix}${i}${vmname_suffix}"
   
-  # EX: Customized VM name:  OCP_Int_Infrax_<IP_oct3>.<IP_oct4>
-  vmname_prefix="OCP_Int_Infra"
+  # EX: Customized VM name:  OCP_Int_Workerx_<IP_oct3>.<IP_oct4>
+  vmname_prefix="OCP_Int_Worker"
   vmname_suffix="$(dig +noall +answer @${dnsserver} +short $vmfqdn | cut -d. -f3,4)"
   vmname="${vmname_prefix}${i}_${vmname_suffix}"
 
